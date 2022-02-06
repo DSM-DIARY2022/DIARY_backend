de16c0299d08a2e6b1a50821cd02ce203a3a415c
최희 <cmlgml094356@gmail.com>
2022-02-06 20:40:44 +0900

    [CREATE] (#1) Entity
    Entity 코드 구현

---
+++ src/main/java/com/example/diary_backend/Entity/Account/AccountRepository.java
@@ -0,0 +1,7 @@
+package com.example.diary_backend.Entity.Account;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface AccountRepository extends CrudRepository<Account, Long> {
+
+}
---
+++ src/main/java/com/example/diary_backend/Entity/Comments/CommentsRepository.java
@@ -0,0 +1,7 @@
+package com.example.diary_backend.Entity.Comments;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface CommentsRepository extends CrudRepository<Comments, Long> {
+
+}
