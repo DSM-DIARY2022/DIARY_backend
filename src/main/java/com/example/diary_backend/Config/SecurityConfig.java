28bb1b0d25361c0e437cc2548259e8ef8e144724
최희 <cmlgml094356@gmail.com>
2022-02-06 20:48:24 +0900

    [CREATE] Security설정파일

---
+++ src/main/java/com/example/diary_backend/Config/SecurityConfig.java
@@ -0,0 +1,36 @@
+package com.example.diary_backend.Config;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+
+@Configuration
+@RequiredArgsConstructor
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter{
+
+    @Bean
+    public BCryptPasswordEncoder passwordEncoder() { // 비밀번호 암호화
+        return new BCryptPasswordEncoder();
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .csrf().disable()
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and()
+                .formLogin().disable()
+                .httpBasic().disable()
+
+                .authorizeRequests();
+
+    }
+
+}
