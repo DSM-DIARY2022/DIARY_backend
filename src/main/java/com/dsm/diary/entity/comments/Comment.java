48e1f21d6494447c3d21e4244abb3c8c1b4d2f0c
최희 <cmlgml094356@gmail.com>
2022-02-09 01:22:02 +0900

    [UPDATE] (#1) 댓글 entity class 이름 변경
    복수형에서 단수형으로 수정

---
+++ src/main/java/com/dsm/diary/entity/comments/Comment.java
@@ -0,0 +1,38 @@
+package com.dsm.diary.entity.comments;
+
+import com.dsm.diary.entity.post.Post;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToOne;
+
+
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+public class Comment { // 댓글
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "comments_id")
+    private Long id;
+
+    // 내용
+    @Column(nullable = false, length = 500)
+    private String content;
+
+    // 게시글
+    @OneToOne(mappedBy = "comments")
+    private Post post;
+
+
+}
