6afecd185bb6d2574cafeccbbbb071fa7be3d2a8
최희 <cmlgml094356@gmail.com>
2022-02-14 22:38:36 +0900

    [DELETE] (#1) 필요없는 import 삭제

--- src/main/java/com/dsm/diary/entity/account/Account.java
+++ src/main/java/com/dsm/diary/entity/account/Account.java
@@ -1,7 +1,7 @@
 package com.dsm.diary.entity.account;
 
-
 import com.dsm.diary.entity.post.Post;
+
 import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
@@ -13,10 +13,8 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.JoinColumn;
-import javax.persistence.ManyToOne;
 import javax.persistence.OneToMany;
-import javax.persistence.OneToOne;
+
 import java.util.List;
 
 @Getter
