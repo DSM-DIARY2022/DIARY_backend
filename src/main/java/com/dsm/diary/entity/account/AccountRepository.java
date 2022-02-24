04342fe77cdd5e4993f3c3d32eec61c0f548a8dd
son <sonjm3518@dsm.hs.kr>
2022-02-24 14:55:00 +0900

    [CREATE](#3) Account Repository

--- src/main/java/com/dsm/diary/entity/account/AccountRepository.java
+++ src/main/java/com/dsm/diary/entity/account/AccountRepository.java
@@ -3,5 +3,10 @@ package com.dsm.diary.entity.account;
 import org.springframework.data.repository.CrudRepository;
 
 public interface AccountRepository extends CrudRepository<Account, Long> {
+<<<<<<< Updated upstream
 
+=======
+    Optional<Account> findByAccountId(String accountId);
+    Optional<Account> findByEmail(String email);
+>>>>>>> Stashed changes
 }
