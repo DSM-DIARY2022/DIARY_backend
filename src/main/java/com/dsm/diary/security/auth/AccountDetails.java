69cd5ac9660920f957993a611af7231f70fdf2e2
son <sonjm3518@dsm.hs.kr>
2022-02-27 02:30:34 +0900

    [CREATE] (#3) UserDetails 사용

---
+++ src/main/java/com/dsm/diary/security/auth/AccountDetails.java
@@ -0,0 +1,51 @@
+package com.dsm.diary.security.auth;
+
+import com.dsm.diary.entity.account.Account;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.Collections;
+
+@Getter
+@AllArgsConstructor
+public class AccountDetails implements UserDetails {
+    private final Account account;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return Collections.emptyList();
+    }
+
+    @Override
+    public String getPassword() {
+        return account.getPassword();
+    }
+
+    @Override
+    public String getUsername() {
+        return account.getEmail();
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+}
