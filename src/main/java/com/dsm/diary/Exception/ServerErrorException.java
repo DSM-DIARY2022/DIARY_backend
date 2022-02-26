531809544b5ba58a05e60d2b7fce4b08b9a1ea58
son <sonjm3518@dsm.hs.kr>
2022-02-27 02:32:43 +0900

    [CREATE] (#3) 500 에러

---
+++ src/main/java/com/dsm/diary/Exception/ServerErrorException.java
@@ -0,0 +1,10 @@
+package com.dsm.diary.Exception;
+
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+public class ServerErrorException extends RuntimeException{
+
+}
