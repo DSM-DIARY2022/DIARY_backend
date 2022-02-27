40d3c2a72a9b163b35e9b3aa842ada9c6d805008
son <sonjm3518@dsm.hs.kr>
2022-02-28 02:36:37 +0900

    [UPDATE] (#3) @Size 수정

--- src/main/java/com/dsm/diary/dto/request/FindAccountIdRequest.java
+++ src/main/java/com/dsm/diary/dto/request/FindAccountIdRequest.java
@@ -6,6 +6,7 @@ import lombok.NoArgsConstructor;
 
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
 
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
@@ -15,5 +16,6 @@ public class FindAccountIdRequest {
     private String email;
 
     @NotBlank(message = "닉네임은 필수 입력 값입니다.")
+    @Size(min = 1, max = 10, message = "닉네임은 1~10글자 이내로 입력해주세요")
     private String name;
 }
