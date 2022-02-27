9fb6434860e6e4ffb19fd9d576837c3613ebc767
son <sonjm3518@dsm.hs.kr>
2022-02-28 02:04:25 +0900

    [UPDATE] (#3) 아이디 찾기 추가

--- src/main/java/com/dsm/diary/service/AuthService.java
+++ src/main/java/com/dsm/diary/service/AuthService.java
@@ -1,12 +1,13 @@
 package com.dsm.diary.service;
 
+import com.dsm.diary.Exception.ServerErrorException;
 import com.dsm.diary.Exception.UnauthorizedException;
 import com.dsm.diary.Facade.AuthFacade;
+import com.dsm.diary.dto.request.FindAccountIdRequest;
 import com.dsm.diary.dto.request.LoginRequest;
 import com.dsm.diary.dto.request.SignupRequest;
 import com.dsm.diary.entity.account.Account;
 import com.dsm.diary.entity.refreshToken.RefreshTokenRepository;
-import com.dsm.diary.security.jwt.JwtProperties;
 import com.dsm.diary.security.jwt.JwtTokenProvider;
 import com.dsm.diary.security.jwt.dto.TokenResponse;
 import lombok.RequiredArgsConstructor;
@@ -20,13 +21,12 @@ public class AuthService {
     private final AuthFacade accountFacade;
     private final PasswordEncoder passwordEncoder;
     private final JwtTokenProvider jwtTokenProvider;
-    private final JwtProperties jwtProperties;
     private final RefreshTokenRepository refreshTokenRepository;
 
     @Transactional
     public void signup(SignupRequest signupRequest){
         accountFacade.signup(signupRequest);
-        accountFacade.sendEmail(signupRequest.getEmail());
+        accountFacade.sendSuccessEmail(signupRequest.getEmail());
     }
 
     @Transactional
@@ -34,7 +34,7 @@ public class AuthService {
         Account account = accountFacade.getByAccountId(loginRequest.getAccountId());
 
         if (!passwordEncoder.matches(loginRequest.getPassword(), account.getPassword())) {
-            throw new IllegalArgumentException("Login Failed");
+            throw new UnauthorizedException();
         }
 
         return jwtTokenProvider.generateToken(account.getAccountId());
@@ -46,5 +46,18 @@ public class AuthService {
                 .filter(token -> jwtTokenProvider.isRefresh(refreshToken))
                 .map(token -> jwtTokenProvider.generateToken(token.getId()))
                 .orElseThrow(UnauthorizedException::new);
+    }
+
+    @Transactional
+    public void findAccountId(FindAccountIdRequest findAccountIdRequest){
+        Account accountEmail = accountFacade.getByEmail(findAccountIdRequest.getEmail());
+        Account accountName = accountFacade.getByName(findAccountIdRequest.getName());
+
+        if(accountEmail.getEmail().equals(findAccountIdRequest.getEmail()) &&
+                accountName.getAccountId().equals(findAccountIdRequest.getName())){
+            accountFacade.sendAccountIdEmail(accountEmail.getEmail(), accountName.getAccountId());
+        } else {
+            throw new UnauthorizedException();
+        }
     }
 }
