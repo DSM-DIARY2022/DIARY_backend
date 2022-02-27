2dae3e46e0b012db78dab9c7597586b32ddecd3c
son <sonjm3518@dsm.hs.kr>
2022-02-28 02:36:06 +0900

    Revert "[UPDATE] (#3) size 수정
    This reverts commit 8c2e0ca0ac9c3d4eafc499493ac47db1cd96c4e1.

--- src/main/java/com/dsm/diary/dto/request/LoginRequest.java
+++ src/main/java/com/dsm/diary/dto/request/LoginRequest.java
@@ -5,18 +5,14 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.Pattern;
 
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class LoginRequest {
     @NotBlank(message = "아이디는 필수 입력 값입니다.")
-    @Pattern(regexp="[a-zA-Z1-9]{5,10}$", message = "아이디는 영문자와 숫자를 모두 포함해서 5~10자리 이내로 입력해주세요.")
     private String accountId;
 
     @NotBlank(message = "비밀번호는 필수 입력 값입니다.")
-    @Pattern(regexp = "(?=.*[0-9])(?=.*[a-zA-Z])(?=.*\\W)(?=\\S+$).{8,16}$",
-            message = "비밀번호는 영문자와 숫자, 특수문자를 모두 포함해서 6~18자리 이내로 입력해주세요.")
     private String password;
 
     /*
