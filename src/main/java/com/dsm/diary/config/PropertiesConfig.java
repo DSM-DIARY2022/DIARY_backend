f62fc416fb1a8150c1968e23e66b09754b0f7149
son <sonjm3518@dsm.hs.kr>
2022-03-07 21:54:53 +0900

    [CREATE] (#3) ConfigurationPropertiesScan을 Bean으로 추가

---
+++ src/main/java/com/dsm/diary/config/PropertiesConfig.java
@@ -0,0 +1,11 @@
+package com.dsm.diary.config;
+
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
+import org.springframework.stereotype.Component;
+
+@Component
+public class PropertiesConfig {
+    public ConfigurationPropertiesScan configurationPropertiesScan() {
+        return configurationPropertiesScan();
+    }
+}
