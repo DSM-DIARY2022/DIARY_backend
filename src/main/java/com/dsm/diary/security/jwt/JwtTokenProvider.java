74382388efccb0b6513dca7c7fd4f7c0a6f5fab4
son <sonjm3518@dsm.hs.kr>
2022-03-07 21:52:14 +0900

    [UPDATE] (#3) type 정해주기

--- src/main/java/com/dsm/diary/security/jwt/JwtTokenProvider.java
+++ src/main/java/com/dsm/diary/security/jwt/JwtTokenProvider.java
@@ -1,8 +1,8 @@
 package com.dsm.diary.security.jwt;
 
-import com.dsm.diary.Exception.UnauthorizedException;
 import com.dsm.diary.entity.refreshToken.RefreshToken;
 import com.dsm.diary.entity.refreshToken.RefreshTokenRepository;
+import com.dsm.diary.exception.UnauthorizedException;
 import com.dsm.diary.security.auth.AccountDetailsService;
 import com.dsm.diary.security.jwt.dto.TokenResponse;
 import io.jsonwebtoken.Claims;
@@ -18,7 +18,6 @@ import org.springframework.stereotype.Component;
 import reactor.util.annotation.Nullable;
 
 import javax.servlet.http.HttpServletRequest;
-import javax.validation.constraints.NotNull;
 import java.util.Date;
 
 @Component
@@ -28,21 +27,25 @@ public class JwtTokenProvider {
     private final AccountDetailsService accountDetailsService;
     private final RefreshTokenRepository refreshTokenRepository;
 
-    public TokenResponse generateToken(String accountId){
-        String access = generateAccessToken(accountId);
-        String refresh =  generateRefreshToken(accountId);
+    public TokenResponse generateToken(String accountId) {
+        String access = generateAccessToken(accountId, "access");
+        String refresh = generateRefreshToken(accountId, "refresh");
 
         refreshTokenRepository.save(
-                new RefreshToken(accountId, refresh)
+                RefreshToken.builder()
+                        .id(accountId)
+                        .refreshToken(refresh)
+                        .ttl(jwtProperties.getRefreshExp())
+                        .build()
         );
 
         return new TokenResponse(access, refresh);
     }
 
-    public String generateAccessToken(String accountId){
+    public String generateAccessToken(String accountId, String type) {
         return Jwts.builder()
                 .setSubject(accountId)
-                .claim("type", "access")
+                .claim("typ", type)
                 .signWith(SignatureAlgorithm.HS256, jwtProperties.getSecretKey())
                 .setExpiration(
                         new Date(System.currentTimeMillis() + jwtProperties.getAccessExp() * 1000)
@@ -51,10 +54,10 @@ public class JwtTokenProvider {
                 .compact();
     }
 
-    public String generateRefreshToken(String accountId){
+    public String generateRefreshToken(String accountId, String type) {
         return Jwts.builder()
                 .setSubject(accountId)
-                .claim("type", "refresh")
+                .claim("typ", type)
                 .signWith(SignatureAlgorithm.HS256, jwtProperties.getSecretKey())
                 .setExpiration(
                         new Date(System.currentTimeMillis() + jwtProperties.getRefreshExp() * 1000)
@@ -63,53 +66,45 @@ public class JwtTokenProvider {
                 .compact();
     }
 
-    public String resolveToken(HttpServletRequest request){
+    public String resolveToken(HttpServletRequest request) {
         String bearerToken = request.getHeader(jwtProperties.getHeader());
-        if(bearerToken != null && bearerToken.startsWith(jwtProperties.getPrefix()) && bearerToken.length() > 7){
+        if (bearerToken != null && bearerToken.startsWith(jwtProperties.getPrefix()) && bearerToken.length() > 7) {
             return bearerToken.substring(7);
         }
         return null;
     }
 
-    public boolean validateToken(String token){
+    public boolean validateToken(String token) {
         return getBody(token)
                 .getExpiration().after(new Date());
     }
 
-    public Authentication getAuthentication(String token){
+    public Authentication getAuthentication(String token) {
         Claims body = getBody(token);
 
-        if (body.getExpiration().before(new Date())){
+        if (body.getExpiration().before(new Date())) {
             throw new UnauthorizedException();
         }
         UserDetails userDetails = getDetails(body);
         return new UsernamePasswordAuthenticationToken(userDetails, "", userDetails.getAuthorities());
     }
 
-    public Claims getBody(String token){
-        if(token == null){
+    public Claims getBody(String token) {
+        if (token == null) {
             throw new UnauthorizedException();
         }
 
-        try{
+        try {
             return Jwts.parser()
                     .setSigningKey(jwtProperties.getSecretKey())
                     .parseClaimsJws(token).getBody();
-        } catch (ExpiredJwtException | MalformedJwtException e){
-            throw new UnauthorizedException();
-        }
-    }
-
-    public boolean isRefresh(String token){
-        try{
-            return getBody(token).get("type").equals("refresh");
-        } catch (ExpiredJwtException | MalformedJwtException e){
+        } catch (ExpiredJwtException | MalformedJwtException e) {
             throw new UnauthorizedException();
         }
     }
 
     @Nullable
-    public UserDetails getDetails(Claims body){
+    public UserDetails getDetails(Claims body) {
         return accountDetailsService.loadUserByUsername(body.getSubject());
     }
 }
