8520c56ffc71f25dc0ecfb151b76644fac8cac2e
son <sonjm3518@dsm.hs.kr>
2022-03-07 21:51:31 +0900

    [UPDATE] (#3) reissue, 아이디 찾기 수정

--- src/main/java/com/dsm/diary/service/AuthService.java
+++ src/main/java/com/dsm/diary/service/AuthService.java
@@ -1,12 +1,16 @@
 package com.dsm.diary.service;
 
-import com.dsm.diary.Exception.UnauthorizedException;
-import com.dsm.diary.Facade.AuthFacade;
 import com.dsm.diary.dto.request.FindAccountIdRequest;
 import com.dsm.diary.dto.request.LoginRequest;
 import com.dsm.diary.dto.request.SignupRequest;
 import com.dsm.diary.entity.account.Account;
+import com.dsm.diary.entity.account.AccountRepository;
+import com.dsm.diary.entity.refreshToken.RefreshToken;
 import com.dsm.diary.entity.refreshToken.RefreshTokenRepository;
+import com.dsm.diary.exception.NotFoundException;
+import com.dsm.diary.exception.UnauthorizedException;
+import com.dsm.diary.facade.AuthFacade;
+import com.dsm.diary.security.jwt.JwtProperties;
 import com.dsm.diary.security.jwt.JwtTokenProvider;
 import com.dsm.diary.security.jwt.dto.TokenResponse;
 import lombok.RequiredArgsConstructor;
@@ -19,17 +23,19 @@ import org.springframework.transaction.annotation.Transactional;
 public class AuthService {
     private final AuthFacade accountFacade;
     private final PasswordEncoder passwordEncoder;
+    private final JwtProperties jwtProperties;
     private final JwtTokenProvider jwtTokenProvider;
+    private final AccountRepository accountRepository;
     private final RefreshTokenRepository refreshTokenRepository;
 
     @Transactional
-    public void signup(SignupRequest signupRequest){
+    public void signup(SignupRequest signupRequest) {
         accountFacade.signup(signupRequest);
         accountFacade.sendSuccessEmail(signupRequest.getEmail());
     }
 
     @Transactional
-    public TokenResponse login(LoginRequest loginRequest){
+    public TokenResponse login(LoginRequest loginRequest) {
         Account account = accountFacade.getByAccountId(loginRequest.getAccountId());
 
         if (!passwordEncoder.matches(loginRequest.getPassword(), account.getPassword())) {
@@ -40,24 +46,20 @@ public class AuthService {
     }
 
     @Transactional
-    public TokenResponse reissue(String refreshToken){
-        return refreshTokenRepository.findByRefreshToken(refreshToken)
-                .filter(token -> jwtTokenProvider.isRefresh(refreshToken))
-                .map(token -> jwtTokenProvider.generateToken(token.getId()))
-                .orElseThrow(UnauthorizedException::new);
+    public TokenResponse reissue(String refreshToken) {
+        RefreshToken updateRefreshToken = refreshTokenRepository.findByRefreshToken(refreshToken)
+                .orElseThrow(NotFoundException::new);
+        updateRefreshToken.update(jwtProperties.getRefreshExp());
+
+        return jwtTokenProvider.generateToken(updateRefreshToken.getId());
     }
 
-    @Transactional
-    public void findAccountId(FindAccountIdRequest findAccountIdRequest){
-        Account accountEmail = accountFacade.getByEmail(findAccountIdRequest.getEmail());
-        Account accountName = accountFacade.getByName(findAccountIdRequest.getName());
+    @Transactional(readOnly = true)
+    public void findAccountId(FindAccountIdRequest findAccountIdRequest) {
+        accountRepository.findByAccountIdAndEmail(findAccountIdRequest.getName(), findAccountIdRequest.getEmail())
+                .orElseThrow(NotFoundException::new);
 
-        if(accountEmail.getEmail().equals(findAccountIdRequest.getEmail()) &&
-                accountName.getAccountId().equals(findAccountIdRequest.getName())){
-            accountFacade.sendAccountIdEmail(accountEmail.getEmail(), accountName.getAccountId());
-        } else {
-            throw new UnauthorizedException();
-        }
+        accountFacade.sendAccountIdEmail(findAccountIdRequest.getName(), findAccountIdRequest.getEmail());
     }
 
     /**
@@ -70,6 +72,6 @@ public class AuthService {
      *            refreshToken을 찾을 수 없으면 404
      * 아이디 찾기 : 성공하면 email로 아이디 전송, 200
      *              메일 전송실패시 500, 이메일과 아이디를 찾을 수 없으면 404
-    *
+     *
      **/
 }
