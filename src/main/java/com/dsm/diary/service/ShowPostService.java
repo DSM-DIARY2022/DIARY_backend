121f76c6fda23af35c5f70d121714892473eb381
최희 <cmlgml094356@gmail.com>
2022-03-14 19:02:39 +0900

    [UPDATE] (#5) 예외처리부분 service코드 변경

--- src/main/java/com/dsm/diary/service/ShowPostService.java
+++ src/main/java/com/dsm/diary/service/ShowPostService.java
@@ -31,7 +31,7 @@ public class ShowPostService {
     **/
     public PostResponse showPost(Long postId) {
         Post post = postRepository.findById(postId)
-                .orElseThrow(NotFoundException::new);
+                .orElseThrow(()-> new NotFoundException("게시글을 찾을 수 없습니다."));
 
         authUtiil.verificationAccount(post.getAccount());
 
